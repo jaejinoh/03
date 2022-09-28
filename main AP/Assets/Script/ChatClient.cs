7a19ef822766d1ad0ecd119783b1dfa00dfd2700
jaejinoh <wowls1231@naver.com>
2022-09-28 23:01:09 +0900

    3주차

---
+++ main AP/Assets/Script/ChatClient.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+using UnityEngine.UI;
+
+public class ChatClient : MonoBehaviour
+{
+    public InputField inputField;
+
+    Socket client;
+    string fromNetThread = "";
+    byte[] buffer = new byte[1024];
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (fromNetThread.Length > 0)
+        {
+            GameObject.Find("UIText").GetComponent<UnityEngine.UI.Text>().text +=  fromNetThread + "\n";
+
+            fromNetThread = "";
+        }
+    }
+
+
+    public void BtnConnect()
+    {
+        {
+            print("클라이언트 접속중");
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            client.BeginConnect("127.0.0.1", 10000, ConnctCallback, null);
+        }
+    }
+
+    void ConnctCallback(IAsyncResult result)
+    {
+        client.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, RecvCallback, null);
+    }
+
+    public void sendMessage()
+    {
+        {
+            string _Message = inputField.text;
+
+            // Byte[] _buffer;
+            var _buffer = System.Text.ASCIIEncoding.ASCII.GetBytes(_Message);
+            client.BeginSend(_buffer, 0, _buffer.Length, SocketFlags.None, SendCallback, null);
+            GameObject.Find("InputField").GetComponent<UnityEngine.UI.InputField>().text = "";
+        }
+    }
+
+    void SendCallback(IAsyncResult result)
+    {
+        int len = client.EndSend(result);
+
+        print("보낸결과 : " + len);
+
+        //client.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, RecvCallback, null);
+    }
+
+
+    void RecvCallback(IAsyncResult result)
+    {
+        int len = client.EndReceive(result);
+        if (len > 0)
+        {
+            string recv = System.Text.ASCIIEncoding.ASCII.GetString(this.buffer, 0, len);
+            
+            fromNetThread = recv;
+        }
+
+        client.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, RecvCallback, null);
+    }
+}
---
+++ main AP/Assets/Script/ChatServer.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+public class ChatServer : MonoBehaviour
+{
+    Socket server;
+    byte[] buffer = new byte[1024];
+    string fromNetThread = "";
+    List<Socket> clients = new List<Socket>();
+
+    private void Awake()
+    {
+        if(this.gameObject.activeInHierarchy)
+           SetServer();
+    }
+
+    void Start()
+    {
+    
+    }
+
+    void SetServer()
+    {
+        server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        server.Bind(new IPEndPoint(IPAddress.Any, 10000));
+        server.Listen(10);
+
+        server.BeginAccept(AcceptCallback, null);
+    }
+
+    void AcceptCallback(IAsyncResult result)
+    {
+        Socket client = server.EndAccept(result);
+        IPEndPoint addr = ((IPEndPoint)client.RemoteEndPoint);
+
+        print(string.Format("{0}, {1}", addr.ToString(), addr.Port.ToString()));
+
+        client.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, RecvCallback, client);
+
+        clients.Add(client);
+
+        server.BeginAccept(AcceptCallback, null);
+    }
+
+    void RecvCallback(IAsyncResult result)
+    {
+        Socket client = (Socket)result.AsyncState;
+        int len = client.EndReceive(result);
+
+        if (len > 0)
+        {
+            string recv = System.Text.ASCIIEncoding.ASCII.GetString(this.buffer, 0, len);
+            IPEndPoint addr = ((IPEndPoint)client.RemoteEndPoint);
+            fromNetThread = string.Format("{0}, : {1} : {2}", addr.Address, addr.Port, recv);
+            print("Server : " + fromNetThread);
+
+            for (int i = 0; i < clients.Count; i++)
+                clients[i].BeginSend(buffer, 0, len,
+                    SocketFlags.None, SendCallback, client);
+        }
+        client.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, RecvCallback, client);
+    }
+
+    void SendCallback(IAsyncResult result)
+    {
+        Socket client = (Socket)result.AsyncState;
+        int len = client.EndSend(result);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
